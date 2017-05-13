93c6b5bd748078bbd97d8f36bca22e3ac1b89d18
Lewuathe <lewuathe@me.com>
2017-05-13 22:39:39 +0900

    Create script for initializing image directory for the latest post

---
+++ initimg.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+LATEST_POST=$(ls -l _posts | tail -n 1 | awk '{print $NF}' | cut -f1 -d'.')
+
+main() {
+  # Create the image directory correspodning to the latest post
+  mkdir -p images/posts/$LATEST_POST
+}
+
+main $*
